7e0397b3347264fb51a583df1fde2af97a92f00d
doug081979 <doug081979@6884be23-2c2a-0410-b59a-f50d4d03ee1a>
2009-04-15 19:57:41 +0000

    git-svn-id: http://cyberaide.googlecode.com/svn/trunk/project/biostatistics/projects@2576 6884be23-2c2a-0410-b59a-f50d4d03ee1a

--- fuzzyCMedoidsCuda/fuzzyCMedoids_kernel.cu
+++ fuzzyCMedoidsCuda/fuzzyCMedoids_kernel.cu
@@ -8,19 +8,31 @@ __device__ void calculateCost(float* d, float* m, float* costs, int index);
 __device__ float calculateDist(int i, int x, float* d, float* m);
 
 __global__ void fuzzyCMedoids(float* data, float* medoids, float* cost) {
-	int start;
-	int end;
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
 	int j = blockIdx.y * blockDim.y + threadIdx.y;
-	int size = NUM_CLUSTERS * NUM_DIMENSIONS;
+	int start;
+	int end;
+
+	start = (i + j * NUM_DATA_POINTS) * STEP_SIZE;
+
+	if (blockIdx.x == (NUM_BLOCKS - 1) && threadIdx.x == (NUM_THREADS - 1)) {
+		end = NUM_DATA_POINTS;
+	}
+	else {
+		end = start + STEP_SIZE;
+	}
+
+	int mSize = NUM_CLUSTERS * NUM_DIMENSIONS;
+	int dSize = (end - start) * NUM_THREADS;
 
 	__shared__ float costs[NUM_CLUSTERS];
 	__shared__ float ourMedoids[NUM_CLUSTERS * NUM_DIMENSIONS];
+	//__shared__ float ourData[];
 
 	if (threadIdx.x == 0) {
-		/*for (int a = 0; a < size; a++) {
+		for (int a = 0; a < mSize; a++) {
 			ourMedoids[a] = medoids[a];
-		}*/
+		}
 
 		for (int z = 0; z < NUM_CLUSTERS; z++) {
 			costs[z] = 0;
@@ -29,25 +41,11 @@ __global__ void fuzzyCMedoids(float* data, float* medoids, float* cost) {
 
 	__syncthreads();
 
-	/*if (ss == 0) {
-		start = i + j * dims[1];
+	if (start < NUM_DATA_POINTS && end <= NUM_DATA_POINTS) {
+		for (int x = start; x < end; x++) {
+			calculateCost(data, ourMedoids, costs, x);
+		}
 	}
-	else {*/
-		start = (i + j * NUM_DATA_POINTS) * STEP_SIZE;
-
-		if (blockIdx.x == (NUM_BLOCKS - 1) && threadIdx.x == (NUM_THREADS - 1)) {
-			end = NUM_DATA_POINTS;
-		}
-		else {
-			end = start + STEP_SIZE;
-		}
-
-		if (start < NUM_DATA_POINTS && end <= NUM_DATA_POINTS) {
-			for (int x = start; x < end; x++) {
-				calculateCost(data, medoids, costs, x);
-			}
-		}
-	//}
 
 	__syncthreads();
 
