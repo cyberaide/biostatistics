e0d53b8fa8fc74d953f588d67d0529e3ad515e61
doug081979 <doug081979@6884be23-2c2a-0410-b59a-f50d4d03ee1a>
2009-04-17 16:05:12 +0000

    git-svn-id: http://cyberaide.googlecode.com/svn/trunk/project/biostatistics/projects@2591 6884be23-2c2a-0410-b59a-f50d4d03ee1a

--- fuzzyCMedoidsCuda/createHeader.sh
+++ fuzzyCMedoidsCuda/createHeader.sh
@@ -5,9 +5,7 @@ NUM_THREADS=$2
 NUM_DATA_POINTS=$3
 NUM_DIMENSIONS=$4
 STEP_SIZE=$5
-BLOCK_DATA_SIZE=$6
-BLOCK_DATA_SIZE_LAST=$7
-DIST_MEASURE=$8
+DIST_MEASURE=$6
 
 cp cmedoids.in.h cmedoids.h
 perl -pi -e "s/#define NUM_CLUSTERS VALUE/#define NUM_CLUSTERS $NUM_CLUSTERS/g" cmedoids.h
@@ -16,5 +14,3 @@ perl -pi -e "s/#define NUM_DATA_POINTS VALUE/#define NUM_DATA_POINTS $NUM_DATA_P
 perl -pi -e "s/#define NUM_DIMENSIONS VALUE/#define NUM_DIMENSIONS $NUM_DIMENSIONS/g" cmedoids.h
 perl -pi -e "s/#define STEP_SIZE VALUE/#define STEP_SIZE $STEP_SIZE/g" cmedoids.h
 perl -pi -e "s/#define DIST_MEASURE VALUE/#define DIST_MEASURE $DIST_MEASURE/g" cmedoids.h
-perl -pi -e "s/#define BLOCK_DATA_SIZE VALUE/#define BLOCK_DATA_SIZE $BLOCK_DATA_SIZE/g" cmedoids.h
-perl -pi -e "s/#define BLOCK_DATA_SIZE_LAST VALUE/#define BLOCK_DATA_SIZE_LAST $BLOCK_DATA_SIZE_LAST/g" cmedoids.h
--- fuzzyCMedoidsCuda/execute.pl
+++ fuzzyCMedoidsCuda/execute.pl
@@ -47,23 +47,29 @@ printf "Number of Clusters = $NUM_CLUSTERS\n";
 # determine number of threads
 #my $NUM_THREADS = 384;
 $NUM_THREADS = 384;
-if($NUM_DIMENSIONS > 8) {
-	if($NUM_DIMENSIONS <= 10){
-		$NUM_THREADS = 320;
-	}
-	elsif($NUM_DIMENSIONS <= 12){
-		$NUM_THREADS = 256;
-	}
-	elsif($NUM_DIMENSIONS <= 16){
-		$NUM_THREADS = 192;
-	}
-	elsif($NUM_DIMENSIONS <= 24){
-		$NUM_THREADS = 128;
-	}
-	else{
-		$NUM_THREADS = 64;
-	}
+
+if ($NUM_THREADS > 512) {
+	$NUM_THREADS = 512;
 }
+
+#if($NUM_DIMENSIONS > 8) {
+#	if($NUM_DIMENSIONS <= 10){
+#		$NUM_THREADS = 320;
+#	}
+#	elsif($NUM_DIMENSIONS <= 12){
+#		$NUM_THREADS = 256;
+#	}
+#	elsif($NUM_DIMENSIONS <= 16){
+#		$NUM_THREADS = 192;
+#	}
+#	elsif($NUM_DIMENSIONS <= 24){
+#		$NUM_THREADS = 128;
+#	}
+#	else{
+#		$NUM_THREADS = 64;
+#	}
+#}
+
 printf "Number of Threads = $NUM_THREADS\n";
 printf "Number of Blocks = $NUM_CLUSTERS\n";
 
@@ -71,18 +77,13 @@ printf "Number of Blocks = $NUM_CLUSTERS\n";
 $STEP_SIZE = int($NUM_DATA_POINTS / ($NUM_CLUSTERS * $NUM_THREADS));
 printf "Thread Step Size = $STEP_SIZE\n";
 
-# thread data size
-$BLOCK_DATA_SIZE = $NUM_DIMENSIONS * $STEP_SIZE;
-$BLOCK_DATA_SIZE_LAST = $NUM_DATA_POINTS - ($STEP_SIZE * $NUM_THREADS * $NUM_CLUSTERS);
-printf "Block Data Size = $BLOCK_DATA_SIZE\n";
-
 # distance measure
 $DIST_MEASURE = $ARGV[2];
 printf "Distance Measure = $DIST_MEASURE\n";
 
 # Create header
 printf "\nCreating header file.\n\n";
-system "./createHeader.sh $NUM_CLUSTERS $NUM_THREADS $NUM_DATA_POINTS $NUM_DIMENSIONS $STEP_SIZE $BLOCK_DATA_SIZE $BLOCK_DATA_SIZE_LAST $DIST_MEASURE";
+system "./createHeader.sh $NUM_CLUSTERS $NUM_THREADS $NUM_DATA_POINTS $NUM_DIMENSIONS $STEP_SIZE $DIST_MEASURE";
 
 # run make
 printf "Cleaning and building the project.\n";
