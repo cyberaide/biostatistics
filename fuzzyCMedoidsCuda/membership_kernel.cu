ed3aa661bbc4bf75cbbdc9c0c545fedb3da97a63
doug081979 <doug081979@6884be23-2c2a-0410-b59a-f50d4d03ee1a>
2009-04-17 02:44:31 +0000

    git-svn-id: http://cyberaide.googlecode.com/svn/trunk/project/biostatistics/projects@2583 6884be23-2c2a-0410-b59a-f50d4d03ee1a

--- fuzzyCMedoidsCuda/membership_kernel.cu
+++ fuzzyCMedoidsCuda/membership_kernel.cu
@@ -13,39 +13,69 @@
 __device__ void calculateMembership(float* d, float* md, float* mb, int m, int index);
 
 __global__ void calcMembership(float* data, float* medoids, float* memb) {
-	int start;
-	int end;
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
 	int j = blockIdx.y * blockDim.y + threadIdx.y;
-	int size = NUM_CLUSTERS * NUM_DIMENSIONS;
+	int start;
+	int end;
 
+	start = (i + j * NUM_DATA_POINTS) * STEP_SIZE;
+
+	if (blockIdx.x == (NUM_BLOCKS - 1) && threadIdx.x == (NUM_THREADS - 1)) {
+		end = NUM_DATA_POINTS;
+	}
+	else {
+		end = start + STEP_SIZE;
+	}
+
+	int mSize = NUM_CLUSTERS * NUM_DIMENSIONS;
+
+	__shared__ float costs[NUM_CLUSTERS];
 	__shared__ float ourMedoids[NUM_CLUSTERS * NUM_DIMENSIONS];
 
+	int count = 0;
+	int dataSize = 0;
+
+	if (blockIdx.x == (NUM_BLOCKS - 1) && threadIdx.x == (NUM_THREADS- 1)) {
+		__shared__ float ourData[BLOCK_DATA_SIZE_LAST];
+
+		for (int x = start; x < end && count < BLOCK_DATA_SIZE_LAST; x++) {
+			for (int y = 0; y < NUM_DIMENSIONS; y++) {
+				ourData[count + y] = data[x + y];
+				count++;
+			}
+		}
+
+		dataSize = BLOCK_DATA_SIZE_LAST;
+		end = NUM_DATA_POINTS;
+	}
+	else if (threadIdx.x == 0) {
+		__shared__ float ourData[BLOCK_DATA_SIZE];
+
+		for (int x = start; x < end && count < BLOCK_DATA_SIZE; x++) {
+			for (int y = 0; y < NUM_DIMENSIONS; y++) {
+				ourData[count + y * BLOCK_DATA_SIZE] = data[x + y * NUM_DATA_POINTS];
+
+			}
+			count++;
+		}
+
+		dataSize = BLOCK_DATA_SIZE_LAST;
+		end = start + STEP_SIZE;
+	}
+
+	start = (i + j * BLOCK_DATA_SIZE) * STEP_SIZE;
+
 	if (threadIdx.x == 0) {
-		for (int a = 0; a < size; a++) {
+		for (int a = 0; a < mSize; a++) {
 			ourMedoids[a] = medoids[a];
 		}
 	}
 
-	/*if (ss == 0) {
-		start = i + j * dims[1];
+	if (start < NUM_DATA_POINTS && end <= NUM_DATA_POINTS) {
+		for (int x = 0; x < dataSize; x++) {
+			calculateMembership(data, ourMedoids, memb, 2, x);
+		}
 	}
-	else {*/
-		start = (i + j * NUM_DATA_POINTS) * STEP_SIZE;
-
-		if (blockIdx.x == (NUM_BLOCKS - 1) && threadIdx.x == (NUM_THREADS - 1)) {
-			end = NUM_DATA_POINTS;
-		}
-		else {
-			end = start + STEP_SIZE;
-		}
-
-		if (start < NUM_DATA_POINTS && end <= NUM_DATA_POINTS) {
-			for (int x = start; x < end; x++) {
-				calculateMembership(data, ourMedoids, memb, 2, x);
-			}
-		}
-	//}
 
 	//__syncthreads();
 }
